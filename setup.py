1cfabec35c39b993ff186be8855343cd5b8503a3
Abhishek Hukkerikar <you@example.com avhukkerikar@gmail.com>
2024-03-26 15:34:26 +0530

    file structure commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)-> List[str]: 
+    requirements = []
+    with open(file_path) as file_obj: 
+        requirements = file_obj.readlines()
+        requirements = [req.replace ("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements: 
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name = "DEMO-PIPELINE", 
+    version = "0.0.1", 
+    author = "Dr. Abhishek V Hukkerikar", 
+    description= "A trial End to End ML project created for First batch of LP students",
+    author_email= "abhishekh@learningpath.ai", 
+    install_requires = get_requirements("requirements.txt"),
+    packages= find_packages())
